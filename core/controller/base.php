4262ea005acc015f9a6d7dbdd3fbe2695f4569b0
Kamil "Brego" Dzielinski <brego.dk@gmail.com>
2008-10-10 21:44:02 -0400

    Modified the behavior of Controller callbacks. They do not return a value now.

--- core/controller/base.php
+++ core/controller/base.php
@@ -40,60 +40,45 @@ class Base {
 	}
 	
 	public function run() {
-		if ($this->before_run() === true) {
+		$this->before_run();
+		$this->before_render();
 			
-			if ($this->before_render() !== true) {
-				die('before_render callback failed');
-			}
+		ob_start();
+		
+		call_user_func_array(array($this, $this->action), $this->params);
 			
-			ob_start();
-			
-			call_user_func_array(array($this, $this->action), $this->params);
-				
-			if ($this->after_run() !== true) {
-				die('after_run callback failed');
-			}
-			
-			extract($this->view_variables);
+		$this->after_run();
+		
+		extract($this->view_variables);
 
-			if (file_exists(::c('VIEWS').$this->shortname.::c('DS').$this->view.'.php') && $this->view !== false) {
-				require_once ::c('VIEWS').$this->shortname.::c('DS').$this->view.'.php';
-			}
-			
-			$content_for_layout = ob_get_clean();
-				
-			if (file_exists(::c('VIEWS').'layouts'.::c('DS').$this->layout.'.php') && $this->layout !== false) {
-				ob_start();
-				require_once ::c('VIEWS').'layouts'.::c('DS').$this->layout.'.php';
-				$output = ob_get_clean();
-			} else {
-				$output = $content_for_layout;
-			}
-			
-			if ($this->after_render($output) !== true) {
-				die('after_render callback failed');
-			}
-				
-		} else {
-			die('before_run callback failed');
+		if (file_exists(::c('VIEWS').$this->shortname.::c('DS').$this->view.'.php') && $this->view !== false) {
+			require_once ::c('VIEWS').$this->shortname.::c('DS').$this->view.'.php';
 		}
+		
+		$content_for_layout = ob_get_clean();
+			
+		if (file_exists(::c('VIEWS').'layouts'.::c('DS').$this->layout.'.php') && $this->layout !== false) {
+			ob_start();
+			require_once ::c('VIEWS').'layouts'.::c('DS').$this->layout.'.php';
+			$output = ob_get_clean();
+		} else {
+			$output = $content_for_layout;
+		}
+		
+		$this->after_render($output);
 	}
 	
 	public function before_run() {
-		return true;
 	}
 	
 	public function after_run() {
-		return true;
 	}
 	
 	public function before_render() {
-		return true;
 	}
 	
 	public function after_render($content = null) {
 		print $content;
-		return true;
 	}
 }
 
