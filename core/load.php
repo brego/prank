85fa49bfa55b77846a4ba111bd05c4cf11f7fef6
Kamil "Brego" Dzielinski <brego.dk@gmail.com>
2008-11-04 00:00:35 -0500

    Simple loading class - thought for loading of helpers. Needs refactoring and tests.

---
+++ core/load.php
@@ -0,0 +1,42 @@
+<?php
+
+class Load {
+	private $storage = array();
+	
+	public function __callStatic($method, $params) {
+		$method = Inflector::singularize($method);
+		if (isset(self::$storage[$method][$params[0]]) === false) {
+			try {
+				if (is_file(c($method).$params[0].'.php')) {
+					require c($method).$params[0].'.php';
+					self::$storage[$method][$params[0]] = true;
+					return true;
+				} elseif (is_file(c()->core.$method.c()->ds.$params[0].'.php')) {
+					require c()->core.$method.c()->ds.$params[0].'.php'
+					self::$storage[$method][$params[0]] = true;
+				} elseif (is_file($params[0].'.php')) {
+					require $params[0].'.php';
+					self::$storage[$method][$params[0]] = true;
+					return true;
+				} else {
+					return false;
+				}
+			} catch (Exception $e) {				
+				if (is_file(c()->core.$method.c()->ds.$params[0].'.php')) {
+					require c()->core.$method.c()->ds.$params[0].'.php'
+					self::$storage[$method][$params[0]] = true;
+				} elseif (is_file($params[0].'.php')) {
+					require $params[0].'.php';
+					self::$storage[$method][$params[0]] = true;
+					return true;
+				} else {
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+	}
+}
+
+?>
