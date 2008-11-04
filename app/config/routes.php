80fc8fdb7a98afd786723247c1bcb6022cbf1368
Kamil "Brego" Dzielinski <brego.dk@gmail.com>
2008-11-03 23:00:31 -0500

    Cleaned up the default route.

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-$map->connect(':controller/:action/:id');
-$map->connect('/', array('controller' => 'default', 'action' => 'index'));
+$map->connect(':controller/:action/:id', array('controller' => 'default', 'action' => 'index'));
 
+// Future:
 //$map->connect(':controller/:action/:id.:format');
 //$map->root(array('controller' => 'welcome'))
 
