995bfeb97e85ff33a5d9bc901c5b8fc4b544fac9
Kamil "Brego" Dzielinski <brego.dk@gmail.com>
2008-11-04 00:01:18 -0500

    Modified test suite to include own recursive rm function.

--- lib/simpletest_prank/prank_test_suite.php
+++ lib/simpletest_prank/prank_test_suite.php
@@ -4,10 +4,41 @@ require_once ROOT.'core'.DS.'base.php';
 
 class PrankTestSuite extends TestSuite {
 	public function __construct($label=false) {
-		rm(ROOT.'tests'.DS.'tmp'.DS);
+		$this->rm(ROOT.'tests'.DS.'tmp'.DS);
 		mkdir(ROOT.'tests'.DS.'tmp'.DS);
 		
 		parent::__construct($label);
+	}
+	
+	private function rm($target) {
+		if (is_file($target)) {
+			if (is_writable($target)) {
+				if (unlink($target)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		if (is_dir($target)) {
+			if (is_writable($target)) {
+				foreach(new DirectoryIterator($target) as $object) {
+					if ($object->isDot()) {
+						unset($object);
+						continue;
+					}
+					if ($object->isFile()) {
+						$this->rm($object->getPathName());
+					} elseif ($object->isDir()) {
+						$this->rm($object->getRealPath());
+					}
+					unset($object);
+				}
+				if (rmdir($target)) {
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 }
 
