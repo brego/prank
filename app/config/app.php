99461984e95596427d75283b31776e8d385fbf87
brego <brego.dk@gmail.com>
2014-06-19 21:19:51 +0200

    Cleanup

--- app/config/app.php
+++ app/config/app.php
@@ -2,11 +2,4 @@
 
 $app['state'] = 'development';
 
-// $app['directories']['models']      = 'models';
-// $app['directories']['views']       = 'views';
-// $app['directories']['controllers'] = 'controllers';
-// $app['directories']['webroot']     = 'webroot';
-// // $app['directories']['config']      = 'config';
-// $app['directories']['helpers']     = 'helpers';
-
 ?>
